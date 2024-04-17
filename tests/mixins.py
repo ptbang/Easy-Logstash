8e01cf068b50a279dde23e8065d3ea69866a8965
Bang Phan <bang.phan@reikiholding.com>
2024-04-17 09:40:24 +0200

    Changed tests- p.2

--- tests/mixins.py
+++ tests/mixins.py
@@ -3,6 +3,6 @@ import pathlib
 
 class TestMixin:
     LOGSTASH_HOST = 'localhost'
-    LOGSTASH_PORT = 5000
+    LOGSTASH_PORT = 50000
     LOGSTASH_DATABASE_PATH = pathlib.Path(__file__).parent / 'logstash.db'
     ELASTICSEARCH_INDEX = 'example-com-test'
